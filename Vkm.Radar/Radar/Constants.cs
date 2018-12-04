38e5d3979f2e11de3d734f7f9476c962f996d45d
Данила Червонный <webmaster.pico@gmail.com>
2018-12-04 19:10:22 +0300

    Рефакторинг

--- Vkm.Radar/Radar/Constants.cs
+++ Vkm.Radar/Radar/Constants.cs
@@ -4,9 +4,9 @@ namespace Vkm.Radar.Radar
 {
     internal class Constants
     {
-        public const double RadarCenterX = 250;
-        public const double RadarCenterY = 250;
         public const double RadarRadius = 250;
+        public const double RadarCenterX = RadarRadius;
+        public const double RadarCenterY = RadarRadius;
 
         public static double RadarDiameter => RadarRadius * 2;
         public static Point RadarCenter => new Point(RadarCenterX, RadarCenterY);
